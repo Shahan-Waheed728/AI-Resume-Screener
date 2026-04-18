afb97d2ab88fa5a27d96f8a97b721de69bd90664
Shahan-Waheed728 <shahanwaheed43@gmail.com>
2026-04-18 12:09:51 +0500

    feat: add preprocessing, feature engineering and model training scripts

--- src/debug_webhook.py
+++ src/debug_webhook.py
@@ -6,9 +6,9 @@ webhook_url = "https://shahan-waheed728.app.n8n.cloud/webhook-test/e852e99d-9807
 test_payload = {
     "candidate_name" : "Fatima Khan",
     "rf_result"      : "Qualified",
-    "ann_score"      : 97.05,
-    "match_score"    : 61.38,
-    "status"         : "HIRE / SHORTLIST",
+    "ml_ann_score"      : 97.05,
+    "ml_match_score"    : 61.38,
+    "ml_status"         : "HIRE / SHORTLIST",
     "timestamp"      : "2026-04-18 07:16:00"
 }
 
